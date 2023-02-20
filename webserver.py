c077cc908c4388b73e58d9fd8acb02b5c964bf24
MAJI-C <jermolenko.m@gmail.com>
2023-02-20 20:16:11 +0100

    Added done path

--- webserver.py
+++ webserver.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, Marina!"
+
+
+@app.route('/done')
+def done():
+    return '<h1>Student Name</h1>'
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=7000, host='0.0.0.0')
